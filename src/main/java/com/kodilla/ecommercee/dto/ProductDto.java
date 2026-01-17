9e0c5867918598f51c9545d9a43e766c0de0640e
Marta Gladysz <kicia.nunus@gmail.com>
2026-01-17 17:50:25 +0100

    ProductController - wstępna implementacja endpointów

---
+++ src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
@@ -0,0 +1,6 @@
+package com.kodilla.ecommercee.dto;
+
+import java.math.BigDecimal;
+
+public record ProductDto(Long id, String name, String description, BigDecimal price, Long  groupId) {
+}
---
+++ src/main/java/com/kodilla/ecommercee/mappper/ProductMapper.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee.mappper;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper {
+}
