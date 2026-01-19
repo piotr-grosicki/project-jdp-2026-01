efb8949c588f3cdca838164f50f34dc1b6003481
Marta <kicia.nunus@gmail.com>
2026-01-19 21:11:39 +0100

    Encja Group - implementacja (#13)

---
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -0,0 +1,29 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "PRODUCTS_GROUPS")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @NotNull
+    @Column(name = "NAME")
+    private String name;
+
+   //@OneToMany(mappedBy = "group", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    //private List<Product> products;
+
+
+}
