c8a48d0eace431960a14daa1ce8c3e8e47b7c1c6
Maciej Kabaj <maciejkabaj93@gmail.com>
2026-01-20 19:45:07 +0100

    JDP202601-12: Added Product entity, repository package and ProductRepository interface (#15)

--- src/main/java/com/kodilla/ecommercee/controller/CartNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartNotFoundException.java
@@ -1,5 +1,7 @@
 package com.kodilla.ecommercee.controller;
 
 public class CartNotFoundException extends RuntimeException {
-
+    public CartNotFoundException(String message) {
+        super(message);
+    }
 }
