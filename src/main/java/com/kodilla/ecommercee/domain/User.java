ec7bbc498f51fcc58252ad398a5571e35709977d
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2026-01-23 12:56:41 +0100

    added a realtion fot carts and order

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -2,7 +2,9 @@ package com.kodilla.ecommercee.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.time.LocalDateTime;
+import java.util.*;
 
 
 @Data
@@ -25,4 +27,22 @@ public class User {
     private String sessionKey;
 
     private LocalDateTime sessionKeyExpiresAt;
+
+    @OneToMany(
+            targetEntity = Cart.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Cart> carts =  new ArrayList<>();
+    /*
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Order> orders =  new ArrayList<>();
+
+     */
 }
